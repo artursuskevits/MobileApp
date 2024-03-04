c090d05eb4c9241d9ba477ce2e1a21be89a13b69
opilane <opilane@HP-CZC2349HTN>
2024-03-04 13:16:31 +0200

    adasdasdad

--- MobileApp/MobileApp/Call_friend.xaml.cs
+++ MobileApp/MobileApp/Call_friend.xaml.cs
@@ -7,21 +7,77 @@ using Plugin.Messaging;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using Xamarin.Forms.Xaml;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace MobileApp
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Call_friend : ContentPage
     {
-        Xamarin.Forms.Entry tel_nr_email,text;
+        TableView tableView;
+        SwitchCell sc;
+        ImageCell ic;
+        TableSection fotosection;
+        EntryCell tel_nr_email,texte,textn,text;
 
         public Call_friend()
         {
-            tel_nr_email = new Xamarin.Forms.Entry { Placeholder = "Kirjuta siia telefoni number" };
-            text = new Xamarin.Forms.Entry { Placeholder = "Kirjuta tekst siia" };
+            sc = new SwitchCell();
+            sc.OnChanged += Sc_OnChanged;
+            ic = new ImageCell
+            {
+                ImageSource = ImageSource.FromFile("urod.jpg"),
+                Text = "FotoNimetus",
+                Detail = "Foto Kirjeldus"
+            };
+            tel_nr_email = new EntryCell()
+            {
+                Label = "Nimi",
+                Placeholder = "Sisesta sõbraga nimi",
+                Keyboard = Keyboard.Default,
+            };
+            textn = new EntryCell()
+            {
+                Label = "Sõnum",
+                Placeholder = "Sisesta sõnum",
+                Keyboard = Keyboard.Default,
+            };
+            texte = new EntryCell()
+            {
+                Label = "Sõnum",
+                Placeholder = "Sisesta sõnum",
+                Keyboard = Keyboard.Default,
+            };
+            fotosection = new TableSection();
+            tableView = new TableView()
+            {
+                Intent = TableIntent.Form,
+                Root = new TableRoot("Andmete sisestamine")
+                {
+                    new TableSection("Põhiandmed: ")
+                    {
+                        tel_nr_email
+                    },
+                    new TableSection("Kontaktiandmed: ")
+                    {
+                        new EntryCell
+                        {
+                            Label = "Telefon",
+                            Placeholder= "Sisesta tel. number",
+                            Keyboard=Keyboard.Telephone
+                        },
+                        new EntryCell
+                        {
+                            Label = "Email",
+                            Placeholder= "Sisesta email",
+                            Keyboard=Keyboard.Email
+                        },
+                        sc
+                    },
+                    fotosection
+                }
+            };
 
-            Button sms_btn = new Button { Text = "Saada SMS"};
+            Button sms_btn = new Button { Text = "Saada SMS" };
             sms_btn.Clicked += Sms_btn_Clicked;
 
             Button call_btn = new Button { Text = "Helista" };
@@ -33,39 +89,61 @@ namespace MobileApp
             Grid actionStackLayout = new Grid
             {
                 HorizontalOptions = LayoutOptions.FillAndExpand,
-                Children = { call_btn, sms_btn, mail_btn},
-                
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+                }
             };
             actionStackLayout.Children.Add(call_btn, 0, 0);
             actionStackLayout.Children.Add(sms_btn, 1, 0);
             actionStackLayout.Children.Add(mail_btn, 2, 0);
-            Content = new StackLayout { Children = {tel_nr_email,text, actionStackLayout } };
 
-
-
-
-
+            Content = new StackLayout { Children = { tableView, actionStackLayout } };
         }
+
+        private void Sc_OnChanged(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+            {
+                fotosection.Title = "Foto";
+                fotosection.Add(ic);
+                sc.Text = "Peida";
+            }
+            else
+            {
+                fotosection.Title = "";
+                fotosection.Remove(ic);
+                sc.Text = "Näita veel";
+            }
+        }
+
         private void Sms_btn_Clicked(object sender, EventArgs e)
         {
             var sms = CrossMessaging.Current.SmsMessenger;
             if (sms.CanSendSms)
             {
-                sms.SendSms(tel_nr_email.Text, text.Text); }
+                sms.SendSms(tel_nr_email.Text, textn.Text);
+            }
         }
 
         private void Call_btn_Clicked(object sender, EventArgs e)
         {
             var call = CrossMessaging.Current.PhoneDialer;
-            if (call.CanMakePhoneCall) {
-                call.MakePhoneCall(tel_nr_email.Text); }
+            if (call.CanMakePhoneCall)
+            {
+                call.MakePhoneCall(tel_nr_email.Text);
+            }
         }
 
         private void Mail_btn_Clicked(object sender, EventArgs e)
         {
             var mail = CrossMessaging.Current.EmailMessenger;
-            if (mail.CanSendEmail){
-                mail.SendEmail(tel_nr_email.Text, "Tervitus!", text.Text); }
+            if (mail.CanSendEmail)
+            {
+                mail.SendEmail(tel_nr_email.Text, "Tervitus!", text.Text);
+            }
         }
     }
 }
